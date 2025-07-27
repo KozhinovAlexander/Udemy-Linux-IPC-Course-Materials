338f3f5f5b77ee5f1c9c0da5e2c3d371522a0a49
Alexander Kozhinov <ak.alexander.kozhinov@gmail.com>
2025-07-27 19:55:43 +0200

    wip

---
+++ 01_unix_domain_sockets/test/test_rtm_server_client.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2025 Alexander Kozhinov <ak.alexander.kozhinov@gmail.com>
+ * SPDX-License-Identifier: Apache-2.0
+ * @brief Routig Table Unit-Tests
+ */
+
+#include <iostream>
+#include <gtest/gtest.h>
+#include <rtm_server.hpp>
+
+
+using namespace RTM;
+
+
+class rtm_server_client_test : public ::testing::Test {
+public:
+	rtm_server srv;
+protected:
+	void SetUp() override
+	{
+	}
+	void TearDown() override
+	{
+	}
+};
+
+
+TEST_F(rtm_server_client_test, foo)
+{
+	EXPECT_EQ(true, false);
+}
